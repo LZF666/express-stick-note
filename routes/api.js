7d056e08a5bb08f8f2478c539191933a3d280b64
LZF666 <850215855@qq.com>
2018-10-10 13:01:32 +0800

    add delete function

--- routes/api.js
+++ routes/api.js
@@ -23,17 +23,48 @@ router.get('/notes', function (req, res, next) {
 router.post('/notes/add', function (req, res, next) {
 
   var note = req.body.note;
+  Note.create({
+    text: note
+  }).then(function () {
+    res.send({
+      status: 0
+    })
+  }).catch(function () {
+    res.send({
+      status: 1,
+      errorMsg: '数据库出错'
+    })
+  })
   console.log('add...', note)
 })
 
 /*修改note*/
 router.post('/notes/edit', function (req, res, next) {
-
+  Note.update({
+    text: req.body.note
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then(function () {
+    console.log(arguments)
+    res.send({
+      status: 0
+    })
+  })
 })
 
 /*删除note*/
 router.post('/notes/delete', function (req, res, next) {
-
+  Note.destroy({
+    where: {
+      id: req.body.id
+    }
+  }).then(function () {
+    res.send({
+      status: 0
+    })
+  })
 })
 
 
